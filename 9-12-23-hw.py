48deaa92a856c8ecd47b1d49018bf0508625577b
techsavv13 <143860279+techsavv13@users.noreply.github.com>
2023-09-12 19:42:45 -0400

    e
    you pisswit

--- 9-12-23-hw.py
+++ 9-12-23-hw.py
@@ -9,3 +9,16 @@ from vpython import *
 import vpvecutils as util
 import numpy as np 
 
+tracc=box(pos=vec(0,1,0),axis=vec(1,0,0),length=2,width=.25,height=.125)
+
+g=util.grav()
+m=.8
+tStep=0.01
+
+Cpos=vec(0,.125,0)
+vi=vec(.5,0,0)
+
+cart=box(pos=Cpos,length=.1,width=.27,height=.125,color=color.purple)
+
+#while Cpos.x()<2:
+    
