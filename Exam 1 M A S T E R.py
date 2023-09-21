c1e3e7acfb21e5bb5af4354b108851e2e3613a30
techsavv13 <143860279+techsavv13@users.noreply.github.com>
2023-09-21 09:46:38 -0400

    Create Exam 1 M A S T E R.py

---
+++ Exam 1 M A S T E R.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 21 09:24:19 2023
+
+@author: Savvie
+
+EXAM 1
+
+SPLIT PROBLEMS OUT
+"""
+
+import vpython as vpy
+import vpvecutils as util
+import numpy as np 
+import math as math
+
