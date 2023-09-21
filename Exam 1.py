74235b8c1ca025f5ef2cb8e1d848c5f9b21718cc
techsavv13 <143860279+techsavv13@users.noreply.github.com>
2023-09-21 09:42:10 -0400

    Exam Start
    From this commit to "Exam End" is what I had access to during Exam 1

---
+++ Exam 1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 21 09:24:19 2023
+
+@author: Savvie
+
+EXAM 1
+"""
+
+import vpython as vpy
+import vpvecutils as util
+import numpy as np 
+import math as math
+
