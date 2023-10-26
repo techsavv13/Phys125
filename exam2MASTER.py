00138832956a3d4ac017735d1c8d4b6de2200c45
techsavv13 <143860279+techsavv13@users.noreply.github.com>
2023-10-26 10:31:49 -0400

    create exam2 fork

---
+++ exam2MASTER.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 10:28:49 2023
+
+@author: Savvie
+"""
+
+from vpython import *
+import vpvecutils as util
+import numpy as np 
+import math as math
+
+
