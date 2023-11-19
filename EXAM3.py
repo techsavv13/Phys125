d7b9d65d4670ef6d3157f404f46f463ab2513279
techsavv13 <143860279+techsavv13@users.noreply.github.com>
2023-11-18 21:01:42 -0500

    jsadbfjdkabjhdnbsdf

--- EXAM3.py
+++ EXAM3.py
@@ -5,5 +5,19 @@ Created on Thu Nov 16 21:04:57 2023
 @author: Savvie
 """
 
-import vpython as vpy
+from vpython import *
 
+
+#q1
+mhe3 = 5.0082*e**-27
+mhe4 = 6.6447*e**-27
+mpro = 1.6726*e**-27
+touchies=1.73*e**-15
+
+startm = mhe3*2
+endm = mhe4+2*mpro
+dm=startm-endm
+
+print(startm)
+print(endm)
+print(dm)
